885c12710bb801fe8239a0607b4c89827935847c
annedube30 <annedube30@gmail.com>
2024-10-02 20:09:27 +0200

    added inner html

--- src/index.js
+++ src/index.js
@@ -26,15 +26,17 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  console.log(cityTimeZone);
 
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = ` <div class="city" id="los-Angeles">
+  let citiesElement = document.querySelector("cities");
+  citiesElement.innerHTML = `<div class="city">
           <div>
             <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM DO YYYY")}</div>
+            <div class="date">${cityTime.format("MMMM D YYYY")}</div>
           </div>
           <div class="time">${cityTime.format(
             "h:mm:ss"
@@ -45,5 +47,5 @@ function updateCity(event) {
 updateTime();
 setInterval(updateTime, 1000);
 
-let citiesSelectElement = document.querySelector("#city");
+let citiesSelectElement = document.querySelector("city");
 citiesSelectElement.addEventListener("change", updateCity);

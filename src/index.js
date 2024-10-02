0ceb3165f7a31bc3ad2bc61ed9dace5347bb7f68
annedube30 <annedube30@gmail.com>
2024-10-02 20:24:05 +0200

    removed a hash tag and replaced it with a dot

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
 
-  let citiesElement = document.querySelector("cities");
+  let citiesElement = document.querySelector(".cities");
   citiesElement.innerHTML = `<div class="city">
           <div>
             <h2>${cityName}</h2>
@@ -47,5 +47,5 @@ function updateCity(event) {
 updateTime();
 setInterval(updateTime, 1000);
 
-let citiesSelectElement = document.querySelector("city");
+let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);

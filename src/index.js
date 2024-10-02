644483e9be34255a9224f53768b1a81aab660bfa
annedube30 <annedube30@gmail.com>
2024-10-02 17:06:17 +0200

    added ids

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function updateTime() {
-  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesElement = document.querySelector("#los-Angeles");
   if (losAngelesElement) {
-    let losAngelesDateElement = losAngelesElement.querySelector("#date");
-    let losAngelesTimeElement = losAngelesElement.querySelector("#time");
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
     let losAngelesTime = moment().tz("America/Los_Angeles");
 
     losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM D YYYY");
@@ -13,8 +13,8 @@ function updateTime() {
 
   let parisElement = document.querySelector("#paris");
   if (parisElement) {
-    let parisDateElement = parisElement.querySelector("#date");
-    let parisTimeElement = parisElement.querySelector("#time");
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
     let parisTime = moment().tz("Europe/Paris");
 
     parisDateElement.innerHTML = parisTime.format("MMMM D YYYY");
@@ -26,18 +26,20 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", "").split("/")[1];
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("cities");
-  citiesElement.innerHTML = `<div class="city">
-          <div>
-            <h2>${cityName}</h2>
-            <div class="date" id="date">${cityTime.format("MMMM DO YYYY")}</div>
-          </div>
-          <div class="time" id="time">${cityTime.format(
-            "h:mm:ss"
-          )}<small>${cityTime.format("A")}</small></div>
-        </div>`;
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 
 updateTime();

66d2db9df230eca8de5cda4741c9a4d381e47159
annedube30 <annedube30@gmail.com>
2024-10-02 13:15:56 +0200

    added update time function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angels");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM DO YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM DO YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
